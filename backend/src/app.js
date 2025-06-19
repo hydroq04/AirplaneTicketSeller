27796890f5d877fd90caa0b87cbbd982ae772658
Hồ Minh Quang <82497662+hydroq04@users.noreply.github.com>
2025-06-19 16:13:17 +0700

    Update database address in app.js
    Database address is now pointed to .env file

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://hmquang:V3jUezo6rzeaSOmI@cluster0.qn1wksd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
